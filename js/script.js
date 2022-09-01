8187513122847779538f75fbf5fc87e2a0997973
Nadia Knofius <nadia.knofius@gmail.com>
2022-09-01 19:59:02 +0200

    priority buttons in add_task

--- js/script.js
+++ js/script.js
@@ -1,7 +1,38 @@
-function toggleClassDNone(id) {
-    document.getElementById(id).classList.toggle('d-none');
+
+
+function toggleClassList(id, classList) {
+    document.getElementById(id).classList.toggle(classList);
 }
 
-function changeValue(value, id){
-    document.getElementById(id).value = value; 
+function changeValue(value, id) {
+    document.getElementById(id).innerHTML = value;
+}
+
+
+// TODO: disable other buttons
+function changePriority(id, classList) {
+    let imgName = `${id}-img`;
+    let imgPath = document.getElementById(imgName);
+    toggleClassList(id, classList);
+    for (let i = 0; i < priority.length; i++) {
+        let prio = priority[i].level;
+        if(prio == id){
+            changePriorityIcons(imgPath, i);
+        }
+    }
+}
+
+function changePriorityIcons(imgPath, i){
+    switch (true){
+        case priority[i].toggle:{
+            priority[i].toggle = false;
+            imgPath.src = priority[i]["img-normal"];
+            break; 
+        }
+        default: {
+            priority[i].toggle = true;
+            imgPath.src = priority[i]["img-choosed"];
+            break; 
+        }
+    }
 }
---
+++ js/templates.js
@@ -0,0 +1,20 @@
+let priority = [
+    {
+        "level": "urgent",
+        "toggle": false,
+        "img-normal": "img/add_task/arrow_urgent.svg",
+        "img-choosed": "img/add_task/arrow_urgent_white.svg",
+    },
+    {
+        "level": "medium",
+        "toggle": false,
+        "img-normal": "img/add_task/medium.svg",
+        "img-choosed": "img/add_task/medium_white.svg",
+    },
+    {
+        "level": "low",
+        "toggle": false,
+        "img-normal": "img/add_task/arrow_low.svg",
+        "img-choosed": "img/add_task/arrow_low_white.svg",
+    }
+];
