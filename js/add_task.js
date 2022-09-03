ba1ddde5c1e7276dec4d96af5ede3bb08823a2fc
Nadia Knofius <nadia.knofius@gmail.com>
2022-09-03 16:10:23 +0200

    outsourced code from script.js to add_task.js // JSDocs added

---
+++ js/add_task.js
@@ -0,0 +1,113 @@
+
+/**
+ * changes value of a dropdownlist
+ * @param {String} value 
+ * @param {String} id 
+ */
+function changeValue(value, id) {
+    document.getElementById(id).innerHTML = value;
+}
+
+
+/**
+ * changes classlist of priority-btns
+ * @param {String} id 
+ * @param {String} classList 
+ */
+function changePriority(id, classList) {
+    let imgName = `${id}-img`;
+    let imgPath = document.getElementById(imgName);
+    toggleClassList(id, classList);
+    for (let i = 0; i < priority.length; i++) {
+        let prio = priority[i].level;
+        if (prio == id) {
+            changePriorityIcons(imgPath, i);
+        }
+    }
+    checkIfBtnIsClicked();
+}
+
+
+/**
+ * changes icons of clicked priority btn
+ * @param {object} imgPath 
+ * @param {number} i 
+ */
+function changePriorityIcons(imgPath, i) {
+    switch (true) {
+        case priority[i].toggle: {
+            priority[i].toggle = false;
+            imgPath.src = priority[i]["img-normal"];
+            break;
+        }
+        default: {
+            priority[i].toggle = true;
+            imgPath.src = priority[i]["img-choosed"];
+            break;
+        }
+    }
+}
+
+
+/**
+ * checks, if a priority-btn is clicked
+ * if yes, calls function for disabling the other btns
+ */
+function checkIfBtnIsClicked() {
+    let counter = 0;
+    for (let i = 0; i < priority.length; i++) {
+        let btnIsClicked = priority[i].toggle;
+        if (btnIsClicked) {
+            disableOtherBtns(i);
+        } else {
+            counter++;
+        }
+    }
+    activateOtherBtns(counter);
+}
+
+
+/**
+ * checks value of index of clicked btn and calls function for disabling the other btns
+ * @param {number} i 
+ */
+function disableOtherBtns(i) {
+    switch (i) {
+        case 0: {
+            deactivateBtns('medium', 'low');
+            break;
+        }
+        case 1: {
+            deactivateBtns('urgent', 'low');
+            break;
+        }
+        case 2: {
+            deactivateBtns('medium', 'urgent');
+            break;
+        }
+    }
+}
+
+
+/**
+ * disables btns
+ * @param {String} id1 //id of first btn to disable
+ * @param {String} id2 //id of second btn to disable
+ */
+function deactivateBtns(id1, id2) {
+    document.getElementById(id1).disabled = true;
+    document.getElementById(id2).disabled = true;
+}
+
+
+/**
+ * activates all priority btns, when counter is equal 3
+ * @param {number} counter 
+ */
+function activateOtherBtns(counter) {
+    if (counter == 3) {
+        document.getElementById('urgent').disabled = false;
+        document.getElementById('medium').disabled = false;
+        document.getElementById('low').disabled = false;
+    }
+}
