663ea25a9cebea2a6a39858d7d5f5e21f2e64bcd
Patrick Frantzen <patrick.frantzen@web.de>
2022-09-06 19:23:42 +0200

    board aktualisiert

---
+++ js/board.js
@@ -0,0 +1,7 @@
+function closeDialog() {
+    document.getElementById('task-display').classList.add('d-none');
+}
+
+function openDialog() {
+    document.getElementById('task-display').classList.remove('d-none');
+}
